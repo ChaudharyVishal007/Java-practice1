0a707d7c38aeff5a5eeb489cfe8a3b1817c69302
Vishal <vishalchaudhary132001@gmail.com>
2024-11-15 19:46:52 +0530

    new files

---
+++ Reversed_String.java
@@ -0,0 +1,11 @@
+package Simple_Programs;
+
+public class Reversed_String {
+    public static void main(String[] args) {
+        String str =" Hello world";
+        String reversed =new StringBuilder(str).reverse().toString();
+        System.out.println(reversed);
+    }
+   
+
+}
